47fc2b963c1ce919aa126fb5e5d4b3c45878b3ca
Chris Munialo <christopher.munialo@gmail.com>
2018-11-14 01:53:42 +0300

    Fix order details
    - Use a computed property to get the current order details
    
    Signed-off-by: Chris Munialo <christopher.munialo@gmail.com>

--- src/modules/transactions/_store/getters.js
+++ src/modules/transactions/_store/getters.js
@@ -4,7 +4,4 @@ export default {
     getOrderHistoryOrders(state){
         return state.order_history_orders;
     },
-    getOrderHistoryOrderDetails: (state) => (order_id) => {
-        return state.order_history_orders.find( order => order.order_no ===  order_id);
-    }
 };
