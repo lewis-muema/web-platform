7233fd593eb102d238ab02bbf69c80ee64a2b28b
Griffin <griffinmuteti31@gmail.com>
2018-11-14 21:50:33 +0300

    element
    - load element on the whole project
    - fix transactions url

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import { createRouter } from './router'
 import { createStore } from './store'
 import { sync } from 'vuex-router-sync'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Element)
 
 // console.log(process.env.NODE_ENV)
 export function createApp () {
