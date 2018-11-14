d675a6a0666ef1b9f7bc04e0673f0e021336bf6b
Griffin <griffinmuteti31@gmail.com>
2018-11-14 11:06:37 +0300

    google key
    - google key

--- src/modules/transactions/_components/OrderDetails.vue
+++ src/modules/transactions/_components/OrderDetails.vue
@@ -56,7 +56,9 @@ export default {
     },
     methods:{
         createStaticMapUrl(from_cordinates, to_cordinates) {
-            return "https://maps.googleapis.com/maps/api/staticmap?path=color:100x1782C5|weight:5|"+from_cordinates+"|"+to_cordinates+"&size=257x257&markers=color:red%7Clabel:P%7C"+from_cordinates+"&markers=color:blue%7Clabel:D%7C "+to_cordinates+ "&key=AIzaSyDJ_S9JgQJSaHa88SXcPbh9JijQOl8RXpc";
+            let google_key = "AIzaSyDJ_S9JgQJSaHa88SXcPbh9JijQOl8RXpc";
+            
+            return "https://maps.googleapis.com/maps/api/staticmap?path=color:100x1782C5|weight:5|"+from_cordinates+"|"+to_cordinates+"&size=257x257&markers=color:red%7Clabel:P%7C"+from_cordinates+"&markers=color:blue%7Clabel:D%7C "+to_cordinates+ "&key="+google_key;
         }
     },
     computed:{
