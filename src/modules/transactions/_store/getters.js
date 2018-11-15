4ca465bab42f4b614bedcd3b934e72db8bc45aa0
Brian Wachira <brian@sendy.co.ke>
2018-11-15 18:30:26 +0300

    Webplatform Remake
    * Fix tables for transactions

--- src/modules/transactions/_store/getters.js
+++ src/modules/transactions/_store/getters.js
@@ -4,4 +4,10 @@ export default {
     getOrderHistoryOrders(state){
         return state.order_history_orders;
     },
+    get_payments(state){
+      return state.payments
+    },
+    get_statement(state){
+      return state.statement
+    },
 };
--- src/modules/transactions/_store/index.js
+++ src/modules/transactions/_store/index.js
@@ -4,6 +4,38 @@ import mutations from './mutations';
 
 const state = {
     order_history_orders : [],
+    payments: [
+      {
+        "Method": "Mpesa",
+        "Txn": "AC31NK346-6DK",
+        "Description": "Order Payment",
+        "Amount": 240,
+        "Date": "2018-11-15 09:54:00",
+      },
+      {
+        "Method": "Mpesa",
+        "Txn": "AC31NK346-6DK",
+        "Description": "Order Payment",
+        "Amount": 240,
+        "Date": "2018-11-15 09:54:00",
+      }
+    ],
+    statement: [
+      {
+        "Method": "Mpesa",
+        "Txn": "AC31NK346-6DK",
+        "Description": "Order Payment",
+        "Amount": 240,
+        "Date": "2018-11-15 09:54:00",
+      },
+      {
+        "Method": "Mpesa",
+        "Txn": "AC31NK346-6DK",
+        "Description": "Order Payment",
+        "Amount": 240,
+        "Date": "2018-11-15 09:54:00",
+      }
+    ],
 };
 
 export default {
