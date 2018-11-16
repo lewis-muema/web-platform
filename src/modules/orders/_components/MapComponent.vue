e265a73611e86fed3d170d27a6e305e4b8ac4ed5
Brian Wachira <brian@sendy.co.ke>
2018-11-16 22:28:32 +0300

    Webplatform Remake
    * Introduce vendor icons
    * Create function to rotate vendors to correct bearing

--- src/modules/orders/_components/MapComponent.vue
+++ src/modules/orders/_components/MapComponent.vue
@@ -3,7 +3,7 @@
     <no-ssr placeholder="">
       <GmapMap :center="{lat:-1.3084143, lng:36.7658132}" :zoom="13" map-type-id="roadmap" class="content" :options="mapOptions">
         <!-- <gmap-marker v-for="m in markers" :position="m.position" :clickable="true" :draggable="true" @click="center=m.position"></gmap-marker> -->
-        <gmap-marker v-for="v in vendors" :position="v.position" :clickable="true" :draggable="true" @click="center=v.position"></gmap-marker>
+        <gmap-marker v-for="v in vendors" :position="v.position" :key="index" :ref="`marker${index}`" :icon="draw_rotated(v.vendor_type,v.rotation)"></gmap-marker>
       </GmapMap>
     </no-ssr>
   </div>
@@ -23,6 +23,46 @@ export default {
       mapOptions: {
         disableDefaultUI : true,
       },
+      markerOptions: {
+
+      }
+    }
+  },
+  methods: {
+    draw_rotated: function(vendor_type, rotation) {
+      var canvas = document.createElement('canvas');
+      canvas.setAttribute("id", "rot_canvas");
+      var ctx = canvas.getContext("2d");
+
+      canvas.width = 100
+      canvas.height = 100
+
+      var image = document.createElement("img");
+      image.crossOrigin = "anonymous"
+      if (vendor_type == 12)
+      {
+        vendor_type = 1
+      }
+      image.src = 'https://images.sendyit.com/web_platform/vendor_type/top/' + vendor_type + '.svg';
+      const imageLoadPromise = new Promise(resolve => {
+        image.onload = resolve
+      }).then(() => {
+        ctx.drawImage(image, 0, 0);
+      })
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.save();
+      ctx.translate(canvas.width / 2, canvas.height / 2);
+      ctx.rotate((rotation - 90) * Math.PI / 180);
+      ctx.drawImage(image, -canvas.width / 2, -canvas.height / 2);
+
+      var rotated = canvas.toDataURL();
+      ctx.restore();
+
+      return {
+        url: rotated,
+        scaledSize: new google.maps.Size(50, 50),
+      };
     }
   },
   computed: {
