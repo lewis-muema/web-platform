09a57aa6d847592a4873149edf6fb2733dc756ce
Dervine <dervinenyakundi@gmail.com>
2018-11-16 22:07:12 +0300

    listing deprtments, API keys and users

--- src/modules/admin/components/API.vue
+++ src/modules/admin/components/API.vue
@@ -1,108 +1,163 @@
 <template lang="html">
     <div class="">
-               <div class="section--filter-wrap">
-        <div class="section--filter-input-wrap">
-        </div>
-        <div class="section--filter-action-wrap">
-          
-          <button type="button" class="button-primary section--filter-action">Update Api Key</button>
-        </div>
-    </div>
+        <div class="section--filter-wrap">
+            <div class="section--filter-input-wrap">
+            </div>
+            <div class="section--filter-action-wrap">
 
-    <el-table
-     :data="tableData"
-     style="width: 100%"
-     :border="true"
-     :stripe="true"
-     >
-     <template slot="empty">
-           {{empty_payments_state}}
-     </template>
-     <el-table-column type="index">
-     </el-table-column>
-     
-     <el-table-column
-       label="Username"
-       prop="username"
-        width="180"
-       >
-     </el-table-column>
-     <el-table-column
-       label="API Key"
-       prop="api_key">
-     </el-table-column>
-     <el-table-column
-       label="Created"
-       prop="created"
-       >
-     </el-table-column>
-     <el-table-column
-       label="API Status"
-       prop="api_status"
-       >
-     </el-table-column>
-     <el-table-column
-       label="Account Status"
-       prop="account_status"
-       >
-     </el-table-column>
-   </el-table>
-   
-    <div class="content">
-        <p><span class="content--bold">Ready to start using the Sendy API?</span> Head over to the <a href="http://docs.sendypublicapi.apiary.io/#" target="_blank">developer site</a>&nbsp;for complete documentation.</p>
-    </div>
+                <button type="button" class="button-primary section--filter-action">{{button_name}}</button>
+            </div>
+        </div>
+        <el-table
+                :data="fetchedData"
+                style="width: 100%"
+                :border="true"
+                :stripe="true"
+        >
+            <template slot="empty">
+                {{empty_payments_state}}
+            </template>
+            <el-table-column type="index">
+            </el-table-column>
+
+            <el-table-column
+                    label="Username"
+                    prop="api_username"
+                    width="180"
+            >
+            </el-table-column>
+            <el-table-column
+                    label="API Key"
+                    prop="api_key">
+            </el-table-column>
+            <el-table-column
+                    label="Created"
+                    prop="api_date_created"
+            >
+            </el-table-column>
+            <el-table-column
+                    label="API Status"
+            >
+                <template slot-scope="scope">
+                    <span>{{ get_api_status(scope.$index) }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column
+                    label="Account Status"
+            >
+                <template slot-scope="scope">
+                    <span>{{ get_account_status(scope.$index) }}</span>
+                </template>
+            </el-table-column>
+        </el-table>
+
+        <div class="content">
+            <p><span class="content--bold">Ready to start using the Sendy API?</span> Head over to the <a
+                    href="http://docs.sendypublicapi.apiary.io/#" target="_blank">developer site</a>&nbsp;for complete
+                documentation.</p>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    name:"API",
-     mounted() {
-    
-    },
-    data:function () {
-      return {
-        empty_payments_state:"Fetching API Credetials",
-        pagination_limit:5,
-        pagination_page:1,
-        tableData:[
-          {
-              "username": "faithshop",
-              "api_key": "zcHUVkH9GtxZmrUyZ6HY",
-              "created": "Oct 30, 2018",
-              "api_status": "Activated",
-              "account_status": "Activated",
-              "key_enviroment": "Live",
-          },
-          {
-              "username": "faithshop",
-              "api_key": "ALVvNhD8d7PVcnadsRSV",
-              "created": "Oct 30, 2018",
-              "api_status": "Activated",
-              "account_status": "Activated",
-              "key_enviroment": "Sandbox",
-          },
-      ]
+    import {mapGetters} from 'vuex'
 
-      }
-    },
-    methods: {
-      updateApiKey() {
-      },
-      changeSize(val) {
-        this.pagination_page = 1;
-        this.pagination_limit = val;
-      },
-      changePage() {
-          console.log('Page changed to', this.pagination_page);
-          let from = (this.pagination_page - 1) * this.pagination_limit;
-          let to = this.pagination_page * this.pagination_limit;
-          let paginated_drivers = this.searched_drivers.slice(from, to);
-          console.log(from, to, paginated_drivers);
-      },
+    export default {
+        name: "API",
+        mounted() {
+            //TODO: Get this from session
+            //TODO: also create payload depending on session
 
+            let payload = {
+                "cop_id": 1083
+            }
+            this.$store.dispatch("$_admin/requestKeysList", payload).then(response => {
+                console.log("Got some data, now lets show something in this component")
+                console.log(response);
+                this.empty_payments_state = "Users List Not Found";
+            }, error => {
+                console.error("Got nothing from server. Prompt user to check internet connection and try again")
+                console.log(error);
+                this.empty_payments_state = "Users List Failed to Fetch";
+            });
+        },
+        data: function () {
+            return {
+                empty_payments_state: "Fetching API Credetials",
+                pagination_limit: 5,
+                pagination_page: 1,
+                button_name: "",
+                tableData: [
+                    {
+                        "username": "faithshop",
+                        "api_key": "zcHUVkH9GtxZmrUyZ6HY",
+                        "created": "Oct 30, 2018",
+                        "api_status": "Activated",
+                        "account_status": "Activated",
+                        "key_enviroment": "Live",
+                    },
+                    {
+                        "username": "faithshop",
+                        "api_key": "ALVvNhD8d7PVcnadsRSV",
+                        "created": "Oct 30, 2018",
+                        "api_status": "Activated",
+                        "account_status": "Activated",
+                        "key_enviroment": "Sandbox",
+                    },
+                ]
+
+            }
+        },
+        computed: {
+            ...mapGetters({
+                fetchedData: '$_admin/getKeysList',
+            }),
+        },
+        methods: {
+            updateApiKey() {
+            },
+            changeSize(val) {
+                this.pagination_page = 1;
+                this.pagination_limit = val;
+            },
+            changePage() {
+                console.log('Page changed to', this.pagination_page);
+                let from = (this.pagination_page - 1) * this.pagination_limit;
+                let to = this.pagination_page * this.pagination_limit;
+                let paginated_drivers = this.searched_drivers.slice(from, to);
+                console.log(from, to, paginated_drivers);
+            },
+            get_account_status: function (index) {
+                let resp = '';
+                if (this.fetchedData.length > 0) {
+                    resp = this.fetchedData[index].account_status;
+                    if (resp === 1) {
+                        resp = "Active"
+                    }
+                    else {
+                        resp = "Deactivated"
+                    }
+                }
+                return resp;
+            },
+            get_api_status: function (index) {
+                let resp = '';
+                if (this.fetchedData.length > 0) {
+                    resp = this.fetchedData[index].api_status;
+                    if (resp === 1) {
+                        resp = "Registered"
+                        this.button_name = "Update API Key"
+                    }
+                    else {
+                        resp = "Not Registered"
+                        this.button_name = "Generate API Key"
+                    }
+                }
+                return resp;
+            },
+
+        }
     }
-}
 </script>
 
 <style lang="css">
--- src/modules/admin/components/users/ListUsers.vue
+++ src/modules/admin/components/users/ListUsers.vue
@@ -1,192 +1,254 @@
 <template lang="html">
-<div>
-    <div class="section--filter-wrap">
-        <div class="section--filter-input-wrap">
-            <input class="input-control section--filter-input" type="text" name="name" value="" placeholder="Name">
-       
-            <el-select class="section--filter-input" v-model="value" placeholder="All Departments">
-                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
-                </el-option>
-            </el-select>
-             </div>
-        <div class="section--filter-action-wrap">
-          <button class="button-primary section--filter-action" @click="addUser">Add User</button>
-          <button type="button" class="button-primary section--filter-action">Search</button>
-          
+    <div>
+        <div class="section--filter-wrap">
+            <div class="section--filter-input-wrap">
+                <input class="input-control section--filter-input" type="text" name="name" value="" placeholder="Name">
+
+                <el-select class="section--filter-input" v-model="value" placeholder="All Departments">
+                    <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+                    </el-option>
+                </el-select>
+            </div>
+            <div class="section--filter-action-wrap">
+                <button class="button-primary section--filter-action" @click="addUser">Add User</button>
+                <button type="button" class="button-primary section--filter-action">Search</button>
+
+            </div>
         </div>
+        <el-table
+                :data="fetchedData"
+                style="width: 100%"
+                :border="true"
+                :stripe="true"
+        >
+            <template slot="empty">
+                {{empty_orders_state}}
+            </template>
+            <el-table-column type="index">
+            </el-table-column>
+            <el-table-column
+                    label="Name"
+                    prop="name"
+                    width="200"
+            >
+            </el-table-column>
+            <el-table-column
+                    label="Phone"
+                    prop="phone"
+                    width="200">
+            </el-table-column>
+
+            <el-table-column
+                    label="Email"
+                    prop="email"
+                    width="220"
+            >
+            </el-table-column>
+            <el-table-column
+                    label="Department"
+                    prop="department_name"
+                    width="180"
+            >
+            </el-table-column>
+            <el-table-column
+                    label="Type"
+                    width="120"
+            >
+                <template slot-scope="scope">
+                    <span>{{ get_user_type(scope.$index) }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column
+                    label="Status"
+                    width="120"
+            >
+                <template slot-scope="scope">
+                    <span>{{ get_user_status(scope.$index) }}</span>
+                </template>
+            </el-table-column>
+            <!--<el-table-column-->
+            <!--label="Branch"-->
+            <!--prop="branch">-->
+            <!--</el-table-column>-->
+            <el-table-column
+                    label="Action">
+                <template slot-scope="scope">
+                    <a>Edit User</a>
+                </template>
+            </el-table-column>
+
+        </el-table>
+
+        <div class="section--pagination-wrap">
+            <el-pagination
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total="fetchedData.length"
+                    :page-size="pagination_limit"
+                    :current-page.sync="pagination_page"
+                    @current-change="changePage"
+                    :page-sizes="[10, 20, 50, 100]"
+                    @size-change="changeSize"
+                    class="section--pagination-item"
+            >
+            </el-pagination>
+        </div>
+
     </div>
-
-     <el-table
-      :data="tableData"
-      style="width: 100%"
-      :border="true"
-      :stripe="true"
-      >
-      <template slot="empty">
-            {{empty_orders_state}}
-      </template>
-      <el-table-column type="index">
-      </el-table-column>
-      <el-table-column
-        label="Name"
-        prop="name"
-         width="180"
-        >
-      </el-table-column>
-      <el-table-column
-        label="Phone"
-        prop="phone">
-      </el-table-column>
-      
-      <el-table-column
-        label="Email"
-        prop="email"
-        width="120"
-        >
-      </el-table-column>
-      <el-table-column
-        label="Department"
-        prop="department"
-        width="120"
-        >
-      </el-table-column>
-      <el-table-column
-        label="Type"
-        prop="type"
-        width="120"
-        >
-      </el-table-column>
-      <el-table-column
-        label="Status"
-        prop="status">
-      </el-table-column>
-      <el-table-column
-        label="Branch"
-        prop="branch">
-      </el-table-column>
-      <el-table-column
-        label="Action">
-        <template slot-scope="scope">
-          <a>Edit User</a>
-        </template>
-      </el-table-column>
-      
-  </el-table>
-
-  <div class="section--pagination-wrap">
-    <el-pagination
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="tableData.length"
-        :page-size="pagination_limit"
-        :current-page.sync="pagination_page"
-        @current-change="changePage"
-        :page-sizes="[10, 20, 50, 100]"
-        @size-change="changeSize"
-        class="section--pagination-item"
-        >
-    </el-pagination>
-</div>
-    
-</div>
 
 </template>
 
 <script>
-export default {
-    name:"ListUsers",
-    mounted() {
-    
-    },
-    data:function () {
-      return {
-        empty_payments_state:"Fetching Users",
-        pagination_limit:5,
-        pagination_page:1,
-        tableData:[
-          {
-              "name": "CASH-AC29TZ828-T4W",
-              "phone": "Computer Architecture",
-              "email": "Computers",
-              "department": "125.60",
-              "type": "24",
-              "status": "24",
-              "branch": "24"
-          },
-          {
-              "name": "CASH-AC29TZ828-T4W",
-              "phone": "Asp.Net 4 Blue Book",
-              "email": "Programming",
-              "department": "56.00",
-              "type": "24",
-              "status": "24",
-              "branch": "24"
-          },
-          {
-              "name": "CASH-AC29TZ828-T4W",
-              "phone": "Popular Science",
-              "email": "Science",
-              "department": "210.40",
-              "type": "27",
-              "status": "27",
-              "branch": "27"
-          }
-      ]
+    import {mapActions, mapGetters} from 'vuex'
 
-      }
-    },
-    methods: {
-      addUser() {
-        this.$router.push({ name: 'adminAddUser'});
-      },
-      changeSize(val) {
-        this.pagination_page = 1;
-        this.pagination_limit = val;
-      },
-      changePage() {
-          console.log('Page changed to', this.pagination_page);
-          let from = (this.pagination_page - 1) * this.pagination_limit;
-          let to = this.pagination_page * this.pagination_limit;
-          let paginated_drivers = this.searched_drivers.slice(from, to);
-          console.log(from, to, paginated_drivers);
-      },
+    export default {
+        name: "ListUsers",
+        mounted() {
+            //TODO: Get this from session
+            //TODO: also create payload depending on session
 
+            let payload = {
+                "cop_id": 1083
+            }
+            this.$store.dispatch("$_admin/requestUsersList", payload).then(response => {
+                console.log("Got some data, now lets show something in this component")
+                console.log(response);
+                this.empty_payments_state = "Users List Not Found";
+            }, error => {
+                console.error("Got nothing from server. Prompt user to check internet connection and try again")
+                console.log(error);
+                this.empty_payments_state = "Users List Failed to Fetch";
+            });
+        },
+        data: function () {
+            return {
+                empty_payments_state: "Fetching Users",
+                pagination_limit: 5,
+                pagination_page: 1,
+                tableData: [
+                    {
+                        "name": "CASH-AC29TZ828-T4W",
+                        "phone": "Computer Architecture",
+                        "email": "Computers",
+                        "department": "125.60",
+                        "type": "24",
+                        "status": "24",
+                        "branch": "24"
+                    },
+                    {
+                        "name": "CASH-AC29TZ828-T4W",
+                        "phone": "Asp.Net 4 Blue Book",
+                        "email": "Programming",
+                        "department": "56.00",
+                        "type": "24",
+                        "status": "24",
+                        "branch": "24"
+                    },
+                    {
+                        "name": "CASH-AC29TZ828-T4W",
+                        "phone": "Popular Science",
+                        "email": "Science",
+                        "department": "210.40",
+                        "type": "27",
+                        "status": "27",
+                        "branch": "27"
+                    }
+                ]
+
+            }
+        },
+        computed: {
+            ...mapGetters({
+                fetchedData: '$_admin/getUsersList',
+            }),
+        },
+
+        methods: {
+            addUser() {
+                this.$router.push({name: 'adminAddUser'});
+            },
+            changeSize(val) {
+                this.pagination_page = 1;
+                this.pagination_limit = val;
+            },
+            changePage() {
+                console.log('Page changed to', this.pagination_page);
+                let from = (this.pagination_page - 1) * this.pagination_limit;
+                let to = this.pagination_page * this.pagination_limit;
+                let paginated_drivers = this.searched_drivers.slice(from, to);
+                console.log(from, to, paginated_drivers);
+            },
+            get_user_type: function (index) {
+                let resp = '';
+                if (this.fetchedData.length > 0) {
+                    resp = this.fetchedData[index].type;
+                    if (resp === 1) {
+                        resp = "Admin"
+                    }
+                    else {
+                        resp = "Normal"
+                    }
+                }
+                return resp;
+            },
+            get_user_status: function (index) {
+                let resp = '';
+                if (this.fetchedData.length > 0) {
+                    resp = this.fetchedData[index].status;
+                    if (resp === 1) {
+                        resp = "Active"
+                    }
+                    else if (resp === 2) {
+                        resp = "Deactivated"
+                    }
+                    else if (resp === 0) {
+                        resp = "Deleted"
+                    }
+                }
+                return resp;
+            },
+            ...mapActions([
+                '$_admin/requestUsersList',
+            ]),
+
+        }
     }
-}
 </script>
 
 <style lang="css" scoped>
-@import 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons';
-/* @import "../../../../assets/styles/datatable.css"; */
+    @import 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons';
+    /* @import "../../../../assets/styles/datatable.css"; */
 
-.button{
-    float:right;
-}
-.button__element{
-    text-transform: uppercase;
-    display: inline-block;
-    padding: 10px 30px;
-    margin-bottom: 0;
-    font-size: 14px;
-    font-weight: normal;
-    line-height: 1.42857143;
-    text-align: center;
-    white-space: nowrap;
-    vertical-align: middle;
-    cursor: pointer;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    background-image: none;
-    border: 1px solid transparent;
-    border-radius: 3px;
-    color: #fff;
-    background-color: #1782C5;
-    border-color: #357ebd;
-    text-decoration: none;
-}
-.menu{
-    display: flow-root;
-}
+    .button {
+        float: right;
+    }
+
+    .button__element {
+        text-transform: uppercase;
+        display: inline-block;
+        padding: 10px 30px;
+        margin-bottom: 0;
+        font-size: 14px;
+        font-weight: normal;
+        line-height: 1.42857143;
+        text-align: center;
+        white-space: nowrap;
+        vertical-align: middle;
+        cursor: pointer;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        background-image: none;
+        border: 1px solid transparent;
+        border-radius: 3px;
+        color: #fff;
+        background-color: #1782C5;
+        border-color: #357ebd;
+        text-decoration: none;
+    }
+
+    .menu {
+        display: flow-root;
+    }
 
 </style>
