3bceacf526e29196df301cac7c7957b977720211
Brian Wachira <brian@sendy.co.ke>
2018-11-16 10:18:09 +0300

    Webplatform Remake
    * Add template setup for markers and polylines

--- src/modules/orders/_store/mutations.js
+++ src/modules/orders/_store/mutations.js
@@ -12,6 +12,14 @@ const set_ongoing_orders = (state, payload) => {
   state.ongoing_orders = payload;
 };
 
+const set_markers = (state, payload => {
+  state.map.markers.push(payload);
+})
+
+const set_polylines = (state, payload => {
+  state.map.polylines.push(payload);
+})
+
 export default {
   toggle_ongoing,
   set_ongoing_orders,
