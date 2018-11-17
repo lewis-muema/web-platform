edcc96d4cbaa08610195ba989a9628bd19435f75
Chris Munialo <christopher.munialo@gmail.com>
2018-11-17 20:18:36 +0300

    Persist note, schedule time
    - Persist note and schedule time
    - Use element calender
    Signed-off-by: Chris Munialo <christopher.munialo@gmail.com>

--- src/modules/orders/_components/order_placement/_store/getters.js
+++ src/modules/orders/_components/order_placement/_store/getters.js
@@ -1,11 +1,12 @@
-const get_waypoints = state => state.waypoints;
-const get_price_request_object = state => state.price_request_object;
-const get_active_package_class = state => state.active_package_class;
-const get_max_way_points = state => state.max_way_points;
-const get_order_locations = state => state.order_locations;
-const get_active_vendor_name = state => state.active_vendor_name;
-const get_active_order_option = state => state.active_order_option;
 
 export default {
-  get_waypoints,get_price_request_object,get_active_package_class,get_max_way_points,get_order_locations,get_active_vendor_name,get_active_order_option
+    get_waypoints: (state) => state.waypoints,
+    get_price_request_object: (state) => state.price_request_object,
+    get_active_package_class: (state) => state.active_package_class,
+    get_max_way_points: (state) => state.max_way_points,
+    get_order_locations: (state) => state.order_locations,
+    get_active_vendor_name: (state) => state.active_vendor_name,
+    get_active_order_option: (state) => state.active_order_option,
+    get_schedule_time: (state) => state.schedule_time,
+    get_order_notes: (state) => state.order_notes,
 };
