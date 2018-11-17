3a9f279bb09f494cf3b86b82fe74da4708b0b948
Clinton <clinton@Clintons-MacBook-Air.local>
2018-11-18 01:29:47 +0300

    Search Logic

--- src/modules/transactions/_components/Statement.vue
+++ src/modules/transactions/_components/Statement.vue
@@ -28,7 +28,7 @@
      </el-table-column>
      <el-table-column
        label="Date"
-       prop="date_time" 
+       prop="date_time"
        :formatter="formatDate">
      </el-table-column>
      <el-table-column
@@ -92,10 +92,10 @@ export default {
         "to_date":""
       },
       filteredStatementData:[]
-      
+
     }
   },
-  mounted(){ 
+  mounted(){
       let statement_payload = {
         "cop_id": 669
       }
@@ -125,26 +125,26 @@ export default {
         return moment(row.date_time).format('MMM Do YYYY, h:mm a');
       },
       formatRunningBalance(row, column, cellValue) {
-         let value = (row.running_balance).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); 
+         let value = (row.running_balance).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
          value = value.split(".");
          return value[0];
       },
       formatDebitAmount(row, column, cellValue) {
          if(Math.sign(row.amount) > 0){
-            
-            let value = (row.amount).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); 
+
+            let value = (row.amount).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
             value = value.split(".");
             return value[0];
-    
+
          }
       },
       formatCreditAmount(row, column, cellValue) {
           if(Math.sign(row.amount) < 0){
-            
-            let value = (row.amount).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); 
+
+            let value = (row.amount).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
             value = value.split(".");
             return value[0];
-    
+
          }
       },
       filterStatementData() {
@@ -154,15 +154,54 @@ export default {
 
             let from_date = this.filterData.from_date;
             let to_date = this.filterData.to_date;
-            this.filteredStatementData = this.statement_data;
-            this.filteredStatementData = this.filteredStatementData.filter(function (statement) {
-              return moment(statement.date_time).isSameOrAfter(from_date) && moment(statement.date_time).isSameOrBefore(to_date);
-            });
-            this.filterState = true;
-            this.empty_statement_state = "Statement Not Found";
+
+            let payload = {
+              "cop_id": 669,
+              "user_type":2,
+              "from":from_date,
+              "to":to_date
+            };
+
+
+            from_date = moment(from_date).format('YYYY-MM-DD');
+            to_date = moment(to_date).format('YYYY-MM-DD');
+
+            this.requestStatement(payload);
+
+            // this.filteredStatementData = this.statement_data;
+            //
+            // console.log(this.filteredStatementData);
+            // console.log(to_date);
+            // if (from_date !== '' && to_date !== '') {
+            //
+            //   this.filteredStatementData = this.filteredStatementData.filter(function (statement) {
+            //     return moment(statement.date_time).isSameOrAfter(from_date) && moment(statement.date_time).isSameOrBefore(to_date);
+            //   });
+            //   this.filterState = true;
+            //
+            // }
+            // this.empty_statement_state = "Statement Not Found";
+      },
+
+      requestStatement(payload){
+          let full_payload = {
+            "values" : payload,
+            "vm":this,
+            "app":"NODE_PRIVATE_API",
+            "endpoint":"statement"
+          }
+          this.$store.dispatch("$_transactions/requestStatement", full_payload).then(response => {
+             console.log("Got some data, now lets show something in this component")
+             console.log(response);
+             this.empty_orders_state = "Order Statement Not Found";
+          }, error => {
+              console.error("Got nothing from server. Prompt user to check internet connection and try again")
+              console.log(error);
+              this.empty_orders_state = "Order Statement Failed to Fetch";
+          });
       }
- 
-      
+
+
   },
   computed: {
     ...mapGetters({
@@ -174,7 +213,7 @@ export default {
     statement_data() {
       let from = (this.pagination_page - 1) * this.pagination_limit;
       let to = this.pagination_page * this.pagination_limit;
-      
+
       if(this.filterState == true){
         return this.filteredStatementData.slice(from, to);
       }
