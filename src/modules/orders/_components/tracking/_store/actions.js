b592d42b9e9a77c6803cbabab5453a91db8ef3a1
Evanson  Biwott <evansonbiwot@gmail.com>
2018-11-22 17:33:41 +0300

    Increase loop time to 50 seconds

--- src/modules/orders/_components/tracking/_store/actions.js
+++ src/modules/orders/_components/tracking/_store/actions.js
@@ -1,50 +1,62 @@
-const get_tracking_data = function({commit, dispatch, state}, data)
-{
-  var payload = {
-    "app":"NODE_PRIVATE_API",
-    "endpoint":"pending_delivery",
-    "values":data
-  }
+const get_tracking_data = function({
+	commit,
+	dispatch,
+	state
+}, data) {
+	var payload = {
+		"app": "NODE_PRIVATE_API",
+		"endpoint": "pending_delivery",
+		"values": data
+	}
 
-  return new Promise((resolve, reject) => {
-    dispatch("requestAxiosPost", payload, { root: true })
-    .then(response => {
-      if (state.tracked_order == data.order_no) {
-        commit('set_tracking_data', response.data)
-        commit('$_orders/set_polyline',response.data.polyline, {root: true})
-        commit('$_orders/set_markers',response.data.path, {root: true})
-        setTimeout(function() {
-          dispatch("get_tracking_data", data)
-        }, 5000);
-      }
-      resolve();
-    }, error => {
-      reject(error);
-      console.log('failed to dispatch to global store')
-    });
-  })
+	return new Promise((resolve, reject) => {
+		dispatch("requestAxiosPost", payload, {
+				root: true
+			})
+			.then(response => {
+				if (state.tracked_order == data.order_no) {
+					commit('set_tracking_data', response.data)
+					commit('$_orders/set_polyline', response.data.polyline, {
+						root: true
+					})
+					commit('$_orders/set_markers', response.data.path, {
+						root: true
+					})
+					setTimeout(function() {
+						dispatch("get_tracking_data", data)
+					}, 50000);
+				}
+				resolve();
+			}, error => {
+				reject(error);
+				console.log('failed to dispatch to global store')
+			});
+	})
 }
 
-const cancel_order = function({commit}, data)
-{
-  var payload = {
-    "app":"PRIVATE_API",
-    "endpoint":"cancel_order",
-    "values":data
-  }
+const cancel_order = function({
+	commit
+}, data) {
+	var payload = {
+		"app": "PRIVATE_API",
+		"endpoint": "cancel_order",
+		"values": data
+	}
 
-  return new Promise((resolve, reject) => {
-    dispatch("requestAxiosPost", payload, { root: true })
-    .then(response => {
-      resolve(response.data);
-    }, error => {
-      reject(error);
-      console.log('failed to dispatch to global store')
-    });
-  })
+	return new Promise((resolve, reject) => {
+		dispatch("requestAxiosPost", payload, {
+				root: true
+			})
+			.then(response => {
+				resolve(response.data);
+			}, error => {
+				reject(error);
+				console.log('failed to dispatch to global store')
+			});
+	})
 }
 
 export default {
-  get_tracking_data,
-  cancel_order
+	get_tracking_data,
+	cancel_order
 };
