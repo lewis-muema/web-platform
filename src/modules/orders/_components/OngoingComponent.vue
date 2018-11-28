0cde35731d8eb80c50ae41b125a29aae7d8f8c28
Brian Wachira <brian@sendy.co.ke>
2018-11-28 15:35:56 +0300

    Increase ongoing order polling to 20 seconds

--- src/modules/orders/_components/OngoingComponent.vue
+++ src/modules/orders/_components/OngoingComponent.vue
@@ -93,7 +93,7 @@ export default {
         if (["order_placement", "tracking"].includes(that.$router.currentRoute.name)) {
           setTimeout(function() {
             that.poll()
-          }, 5000);
+          }, 20000);
         }
         that.loading = false
       })
