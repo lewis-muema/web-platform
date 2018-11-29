0b189f29b865ffe044398a12de1d378039567132
Griffin <griffinmuteti31@gmail.com>
2018-11-29 19:25:01 +0300

    - clear document cookie

--- src/components/headers/MainHeader.vue
+++ src/components/headers/MainHeader.vue
@@ -86,11 +86,6 @@ export default {
         domain +
         "; path=" +
         path;
-
-      document.cookie =
-        name +
-        "=;expires=Thu, 01 Jan 1970 00:00:00 GMT domain=localhost; path=" +
-        path;
     },
     isEmpty(obj) {
       for (var prop in obj) {
