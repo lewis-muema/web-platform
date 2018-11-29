a96a4fbd45fac39f0e62e762c5116351199c9c16
Griffin <griffinmuteti31@gmail.com>
2018-11-29 17:20:22 +0300

    Revert "Add eslint"
    This reverts commit 202e25c892b546fb27f64f9b86a50c86c6715606.

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -5,9 +5,9 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
-process.env.DOCKER_ENV = process.env.DOCKER_ENV || 'development';
-
-const isProd = process.env.NODE_ENV === 'production'
+process.env.NODE_ENV = process.env.DOCKER_ENV || 'production';
+process.env.DOCKER_ENV = process.env.DOCKER_ENV || 'testing';
+const isProd = process.env.NODE_ENV !== 'development'
 
 const env = process.env.DOCKER_ENV === 'testing'
   ?  require('../configs/test.env')
@@ -32,15 +32,6 @@ module.exports = {
   module: {
     noParse: /es6-promise\.js$/, // avoid webpack shimming process
     rules: [
-     // {
-     //     enforce: "pre",
-     //     test: /\.(js|vue)$/,
-     //     exclude: /node_modules/,
-     //     loader: "eslint-loader",
-     //     options: {
-     //       formatter: require('eslint-friendly-formatter')
-     //     }
-     //  },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
