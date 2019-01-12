7eed08ea89f745089cabf400fca4ed10dca35158
Unknown <dervinenyakundi@gmail.com>
2019-01-12 15:01:22 +0300

    Auth
    Handle external endpoints

--- src/store/actions.js
+++ src/store/actions.js
@@ -45,7 +45,7 @@ export default {
                 };
                 
             }
-            else if (payload.endpoint == "sign_up_check"){
+            else if (payload.endpoint == "sign_up_check" || payload.endpoint == "sign_in"){
                 config = {
                     headers: {
                         "Content-Type": "application/json"
@@ -71,7 +71,7 @@ export default {
                 }};
                 
             }
-            else if (payload.endpoint == "sign_up_check"){
+            else if (payload.endpoint == "sign_up_check" || payload.endpoint == "sign_in"){
                 config = {
                     headers: {
                         "Content-Type": "text/plain"
