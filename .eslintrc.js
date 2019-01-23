2d2e45a5af8ba3c0b59ff64f8e8aed001a70c142
Brian Wachira <brian@sendy.co.ke>
2019-01-23 15:31:27 +0300

    Resolve conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,5 @@ module.exports = {
   ],
   rules: {
     "no-debugger": process.env.NODE_ENV === 'production' ? 2 : 0,
-<<<<<<< HEAD
-  },
-=======
   }
->>>>>>> a9e8bad1725e0f3dbd35b0eadde94e22da4bbafc
 }
