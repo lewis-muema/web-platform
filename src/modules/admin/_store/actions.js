87b49a442129e7b7fe5c6a7b892ea38fa9322459
Clinton <clinton@Clintons-MacBook-Air.local>
2019-01-31 15:07:26 +0300

    Change cop details

--- src/modules/admin/_store/actions.js
+++ src/modules/admin/_store/actions.js
@@ -193,4 +193,19 @@ export default {
        })
    },
 
+   requestCopInfo({commit, dispatch}, payload)
+  {
+    return new Promise((resolve, reject) => {
+
+     dispatch('requestAxiosPost', payload, {root: true}).then(response => {
+            console.log('in store dispatch to global store')
+              console.log(response);
+                resolve(response.data);
+         }, error => {
+            reject(error);
+            console.log('failed to dispatch to global store')
+         });
+    })
+  },
+
 };
