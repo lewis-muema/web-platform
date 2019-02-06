391e62b952f002bb09bebdc3fe3340ec19b2ed9d
Griffin <griffinmuteti31@gmail.com>
2019-02-06 15:43:13 +0300

    Revert "merge production"
    This reverts commit 8cd79cac0463bae7146db807a0107170fb689341.

--- src/modules/payment/_store/getters.js
+++ src/modules/payment/_store/getters.js
@@ -1,20 +1,10 @@
 export default {
-	getMpesaLoadingStatus(state) {
-		return state.mpesa_loading_status;
-	},
-	getMpesaFailStatus(state) {
-		return state.mpesa_fail_status;
-	},
-	getMpesaSuccessStatus(state) {
-		return state.mpesa_success_status;
-	},
-	getCardLoadingStatus(state) {
-		return state.card_loading_status;
-	},
-	getCardFailStatus(state) {
-		return state.card_fail_status;
-	},
-	getCardSuccessStatus(state) {
-		return state.card_success_status;
-	},
+  getMpesaLoadingStatus: state => state.mpesa_loading_status,
+  getMpesaFailStatus: state => state.mpesa_fail_status,
+  getMpesaSuccessStatus: state => state.mpesa_success_status,
+  getCardLoadingStatus: state => state.card_loading_status,
+  getCardFailStatus: state => state.card_fail_status,
+  getCardSuccessStatus: state => state.card_success_status,
+  getSavedCards: state => state.saved_cards,
+  getStripeUserId: state => state.stripe_user_id,
 };
