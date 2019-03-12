de184e08c0c6b9ff6d66478c88f9a2f1c8a85744
Clinton <clinton@Clintons-MacBook-Air.local>
2019-03-12 15:51:24 +0300

    Airbnb Standards :Webpack config

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,96 +1,95 @@
-const path = require("path");
-const webpack = require("webpack");
-const vueConfig = require("./vue-loader.config");
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin");
+import path from 'path';
+import webpack from 'webpack';
+import VueLoaderPlugin from 'vue-loader/lib/plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import FriendlyErrorsPlugin from 'friendly-errors-webpack-plugin';
+import vueConfig from './vue-loader.config';
 
-process.env.NODE_ENV = process.env.DOCKER_ENV || "development";
-process.env.DOCKER_ENV = process.env.DOCKER_ENV || "development";
-const isProd = process.env.NODE_ENV !== "development";
+process.env.NODE_ENV = process.env.DOCKER_ENV || 'development';
+process.env.DOCKER_ENV = process.env.DOCKER_ENV || 'development';
+const isProd = process.env.NODE_ENV !== 'development';
 
-const env =
-  process.env.DOCKER_ENV === "testing"
-    ? require("../configs/test.env")
-    : process.env.DOCKER_ENV === "production"
-    ? require("../configs/prod.env")
-    : require("../configs/dev.env");
+const env = process.env.DOCKER_ENV === 'testing'
+  ? require('../configs/test.env')
+  : process.env.DOCKER_ENV === 'production'
+    ? require('../configs/prod.env')
+    : require('../configs/dev.env');
 
 module.exports = {
-  devtool: isProd ? false : "#cheap-module-source-map",
+  devtool: isProd ? false : '#cheap-module-source-map',
   output: {
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "/dist/",
-    filename: "[name].[chunkhash].js"
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/dist/',
+    filename: '[name].[chunkhash].js',
   },
   resolve: {
     alias: {
-      public: path.resolve(__dirname, "../public")
-    }
+      public: path.resolve(__dirname, '../public'),
+    },
   },
   module: {
     noParse: /es6-promise\.js$/, // avoid webpack shimming process
     rules: [
       {
         test: /\.vue$/,
-        loader: "vue-loader",
-        options: vueConfig
+        loader: 'vue-loader',
+        options: vueConfig,
       },
       {
         test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/
+        loader: 'babel-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: "url-loader",
+        loader: 'url-loader',
         options: {
           limit: 10000,
-          name: "[name].[ext]?[hash]"
-        }
+          name: '[name].[ext]?[hash]',
+        },
       },
       {
         test: /\.css$/,
         use: isProd
           ? ExtractTextPlugin.extract({
-              use: "css-loader?minimize",
-              fallback: "vue-style-loader"
-            })
-          : ["vue-style-loader", "css-loader"]
+            use: 'css-loader?minimize',
+            fallback: 'vue-style-loader',
+          })
+          : ['vue-style-loader', 'css-loader'],
       },
       {
         test: /\.(ttf|eot|woff|woff2)$/,
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            name: "fonts/[name].[ext]"
-          }
-        }
-      }
-    ]
+            name: 'fonts/[name].[ext]',
+          },
+        },
+      },
+    ],
   },
   performance: {
     maxEntrypointSize: 300000,
-    hints: isProd ? "warning" : false
+    hints: isProd ? 'warning' : false,
   },
-  mode: process.env.DOCKER_ENV == "development" ? "development" : "production",
+  mode: process.env.DOCKER_ENV === 'development' ? 'development' : 'production',
   plugins: isProd
     ? [
-        new webpack.DefinePlugin({
-          "process.env.CONFIGS_ENV": env
-        }),
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new ExtractTextPlugin({
-          filename: "common.[chunkhash].css",
-          allChunks: true
-        }),
-        new VueLoaderPlugin()
-      ]
+      new webpack.DefinePlugin({
+        'process.env.CONFIGS_ENV': env,
+      }),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new ExtractTextPlugin({
+        filename: 'common.[chunkhash].css',
+        allChunks: true,
+      }),
+      new VueLoaderPlugin(),
+    ]
     : [
-        new webpack.DefinePlugin({
-          "process.env.CONFIGS_ENV": env
-        }),
-        new FriendlyErrorsPlugin(),
-        new VueLoaderPlugin()
-      ]
+      new webpack.DefinePlugin({
+        'process.env.CONFIGS_ENV': env,
+      }),
+      new FriendlyErrorsPlugin(),
+      new VueLoaderPlugin(),
+    ],
 };
