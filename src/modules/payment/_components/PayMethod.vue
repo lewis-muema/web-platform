7f1ad5f79e50923afbb535bf7f5701b74d98d4b8
Clinton <clinton@Clintons-MacBook-Air.local>
2019-04-29 12:01:08 +0300

    Accept card and postpay in Uganda

--- src/modules/payment/_components/PayMethod.vue
+++ src/modules/payment/_components/PayMethod.vue
@@ -1,29 +1,27 @@
 <template lang="html">
   <div class="paymethod">
-    <router-link
-      class="paymethod--link"
-      to="/payment/mpesa"
-    >
+    <router-link v-if="getCountryCode === 'KE'" class="paymethod--link" to="/payment/mpesa">
       M-Pesa
     </router-link>
-    <router-link
-      class="paymethod--link"
-      to="/payment/card"
-    >
+    <router-link class="paymethod--link" to="/payment/card">
       Card
     </router-link>
-    <router-link
-      class="paymethod--link"
-      to="/payment/promo"
-    >
+    <router-link class="paymethod--link" to="/payment/promo">
       Promo Code
     </router-link>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'PayMethod',
+  computed: {
+    ...mapGetters({
+      getCountryCode: 'getCountryCode',
+    }),
+  },
 };
 </script>
 
