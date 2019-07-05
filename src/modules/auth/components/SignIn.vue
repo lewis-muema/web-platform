e2566e8657f3c2e7188885a1067cec55ce8b2bf1
Clinton Mutindi <clinton@sendy.co.ke>
2019-07-05 11:42:53 +0300

    Fix Browser back button issue

--- src/modules/auth/components/SignIn.vue
+++ src/modules/auth/components/SignIn.vue
@@ -89,6 +89,7 @@ export default {
   },
   mounted() {
     this.landOnSignInPage();
+    this.clearSession();
   },
   methods: {
     ...mapActions({
@@ -100,6 +101,11 @@ export default {
         page: location.pathname,
         title: 'View Page - Sign In - Web Platform',
       });
+    },
+    clearSession() {
+      this.$store.commit('deleteSession');
+      localStorage.removeItem('_sessionSnack');
+      localStorage.removeItem('jwtToken');
     },
     sign_in() {
       if (this.email !== '' && this.password !== '') {
