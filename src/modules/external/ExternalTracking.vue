9ead0c5195aa52dca40f5ebe96846bc30609f8f9
Clinton Mutindi <clinton@sendy.co.ke>
2019-07-05 13:16:56 +0300

    External Tracking Page

--- src/modules/external/ExternalTracking.vue
+++ src/modules/external/ExternalTracking.vue
@@ -1,21 +1,18 @@
 <template lang="html">
-  <div
-    id="orders_container"
-    class="box"
-  >
+  <div class="">
     <external-header />
-    <map-component />
-    <transition
-      name="fade"
-      mode="out-in"
-    >
-      <router-view />
-    </transition>
+
+    <div id="orders_container" class="box">
+      <map-component />
+      <transition name="fade" mode="out-in">
+        <router-view />
+      </transition>
+    </div>
   </div>
 </template>
 
 <script>
-import orderStore from '../orders/_store';
+import order_store from '../orders/_store';
 import ExternalHeader from '../../components/headers/ExternalHeader.vue';
 import MapComponent from '../orders/_components/MapComponent.vue';
 
@@ -38,7 +35,7 @@ export default {
     registerOrdersStore() {
       const moduleIsRegistered = this.$store._modules.root._children.$_orders !== undefined;
       if (!moduleIsRegistered) {
-        this.$store.registerModule('$_orders', orderStore);
+        this.$store.registerModule('$_orders', order_store);
       }
     },
   },
