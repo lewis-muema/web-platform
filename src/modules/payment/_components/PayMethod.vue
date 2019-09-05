1b4fa97368826a977dd766d1fe293aad66757ab7
Unknown <dervinenyakundi@gmail.com>
2019-09-05 10:22:28 +0300

    - initial payment options service integration

--- src/modules/payment/_components/PayMethod.vue
+++ src/modules/payment/_components/PayMethod.vue
@@ -7,7 +7,10 @@
     >
       M-Pesa
     </router-link>
-    <router-link class="paymethod--link" to="/payment/card">
+    <router-link
+      class="paymethod--link"
+      to="/payment/card"
+    >
       Card
     </router-link>
     <router-link
@@ -21,7 +24,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'PayMethod',
@@ -36,21 +39,65 @@ export default {
       getCountryCode: 'getCountryCode',
     }),
   },
-  methods: {
-    checkUserPhone() {
-      let session = this.$store.getters.getSession;
-      let phone = session[session.default]['user_phone'];
-      this.currency = session[session.default]['default_currency'];
-      let int_value = phone.substring(0, 4);
-      if (int_value === '+256') {
-        this.mpesa_valid = false;
-      } else {
-        this.mpesa_valid = true;
-      }
-    },
-  },
   created() {
     this.checkUserPhone();
+    this.getPaymentOptions();
+  },
+  methods: {
+    ...mapActions({ requestPaymentOptionsAction: '$_payment/requestPaymentOptions' }),
+
+    checkUserPhone() {
+      const session = this.$store.getters.getSession;
+      const phone = session[session.default].user_phone;
+      this.currency = session[session.default].default_currency;
+      const intValue = phone.substring(0, 4);
+      this.mpesa_valid = intValue !== '+256';
+    },
+    getPaymentOptions() {
+      const session = this.$store.getters.getSession;
+      console.log('tete', session);
+      const countryCode = session[session.default].country_code;
+      let accountType = '';
+      const payOption = session[session.default].pay_option;
+      if (session.default === 'biz') {
+        if (payOption === 1) {
+          accountType = 'Business Pre-Pay';
+        } else if (payOption === 2) {
+          accountType = 'Business Post-Pay';
+        }
+      } else if (session.default === 'peer') {
+        accountType = 'Individual';
+      }
+      const payload = {
+        country_code: countryCode,
+        account_type: accountType,
+        entry_point: 'Customer App Top Up',
+      };
+      const fullPayload = {
+        values: payload,
+        vm: this,
+        app: 'PAYMENT_SERVICE',
+        endpoint: 'accounts/pay_method',
+      };
+      // const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+      // axios
+      // eslint-disable-next-line max-len
+      //   .post(`${proxyurl}${process.env.VUE_APP_PAYMENT_SERVICE}getaccounttypepaymentmethods`, payload, this.service_config)
+      //   .then((response) => {
+      //     this.payment_methods = JSON.parse(JSON.stringify(response.data.payment_methods));
+      //   })
+      //   .catch((error) => {
+      //     this.errorObj = error.response;
+      //   });
+      this.requestPaymentOptionsAction(fullPayload).then(
+        (response) => {
+          console.log('response', response);
+        },
+        (error) => {
+          console.log('error', error);
+        },
+      );
+    },
   },
 };
 </script>
--- src/modules/payment/_store/actions.js
+++ src/modules/payment/_store/actions.js
@@ -189,4 +189,16 @@ export default {
       );
     });
   },
+  requestPaymentOptions({ dispatch }, payload) {
+    return new Promise((resolve, reject) => {
+      dispatch('requestAxiosPost', payload, { root: true }).then(
+        (response) => {
+          resolve(response.data);
+        },
+        (error) => {
+          reject(error);
+        },
+      );
+    });
+  },
 };
