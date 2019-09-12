ddd9f58b46fef5f3ac8eb9af382873151890bfe3
Clinton Mutindi <clinton@sendy.co.ke>
2019-09-12 12:25:11 +0300

    Scheduled Order Timeline

--- src/modules/orders/_components/order_placement/_components/PaymentOptions.vue
+++ src/modules/orders/_components/order_placement/_components/PaymentOptions.vue
@@ -353,6 +353,9 @@ export default {
         .add(this.activeVendorPriceData.eta, 'second')
         .format('YYYY-MM-DD HH:mm:ss');
     },
+    current_time() {
+      return this.moment().format('YYYY-MM-DD HH:mm:ss');
+    },
 
     scheduled_time() {
       return this.moment(this.get_schedule_time, 'YYYY-MM-DD HH:mm:ss Z').format(
@@ -670,7 +673,7 @@ export default {
           : this.payment_method === ''
             ? 0
             : Number(this.payment_method),
-        schedule_time: this.order_is_scheduled ? this.scheduled_time : this.eta_time,
+        schedule_time: this.order_is_scheduled ? this.scheduled_time : this.current_time,
         tier_tag: this.activeVendorPriceData.tier_tag,
         tier_name: this.activeVendorPriceData.tier_name,
         cop_id: 'cop_id' in acc ? acc.cop_id : 0,
