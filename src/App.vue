3762ee00213ff759a30b440de34382ce3a90d93f
Griffin <griffinmuteti31@gmail.com>
2019-11-28 17:06:30 +0300

    - disable internal redirects

--- src/App.vue
+++ src/App.vue
@@ -135,15 +135,15 @@ export default {
         this.$store.commit('setNotificationStatus', true);
 
         // redirect to tracking page when order no has been provided
-
-        if (orderNo !== undefined) {
-          this.$router.push({
-            name: 'tracking',
-            params: {
-              order_no: orderNo,
-            },
-          });
-        }
+        // TODO : create new logic for internal redirects
+        // if (orderNo !== undefined) {
+        //   this.$router.push({
+        //     name: 'tracking',
+        //     params: {
+        //       order_no: orderNo,
+        //     },
+        //   });
+        // }
 
         // TODO: fire different events to act on message recieved
         // proposed central notifications actor class to process different types of notifiacations
