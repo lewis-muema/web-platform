b6396d5b3e06334d566ba0db1491baf998a79741
Griffin <griffinmuteti31@gmail.com>
2019-11-28 13:09:16 +0300

    claim

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,22 +2,22 @@
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.
 importScripts('https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.4.0/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
 firebase.initializeApp({
   messagingSenderId: '1083459536124',
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
 const messaging = firebase.messaging();
 let notificationData = {};
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
 
 self.addEventListener('push', (event) => {
   notificationData = JSON.parse(event.data.text());
@@ -36,29 +36,25 @@ self.addEventListener('notificationclick', (event) => {
   const orderNo = notificationData.data.order_no;
 
   event.waitUntil(
-    clients
-      .matchAll({
-        includeUncontrolled: true,
-      })
-      .then((allClients) => {
-        let sendyClient = false;
+    self.clients.matchAll({ includeUncontrolled: true, type: 'window' }).then((allClients) => {
+      let sendyClient = false;
 
-        for (const client of allClients) {
-          const url = new URL(client.url);
-
-          if (url.origin === `${origin}`) {
-            // Excellent, let's use it!
-            client.focus();
-            sendyClient = true;
-            event.notification.close();
-            break;
-          }
+      for (const client of allClients) {
+        const url = new URL(client.url);
+        if (url.origin === `${origin}`) {
+          client
+            .navigate(`${origin}/orders/tracking/${orderNo}`)
+            .then(webClient => webClient.focus());
+          sendyClient = true;
+          event.notification.close();
+          break;
         }
+      }
 
-        if (!sendyClient) {
-          event.waitUntil(clients.openWindow(`${origin}/orders/tracking/${orderNo}`));
-        }
-        event.notification.close();
-      }),
+      if (!sendyClient) {
+        event.waitUntil(clients.openWindow(`${origin}/orders/tracking/${orderNo}`));
+      }
+      event.notification.close();
+    }),
   );
 });
