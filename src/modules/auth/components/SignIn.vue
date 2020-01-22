6e6f8f3abfcb925a146da3b35b461f91133df295
lewism <lewis@sendy.co.ke>
2020-01-22 15:41:32 +0300

    include apm in sign in component

--- src/modules/auth/components/SignIn.vue
+++ src/modules/auth/components/SignIn.vue
@@ -123,7 +123,7 @@ export default {
           endpoint: 'sign_in',
         };
         this.authSignIn(fullPayload).then(
-          (response) => {
+          response => {
             if (Object.prototype.hasOwnProperty.call(response, 'status')) {
               const errorResponse = response.data;
               if (errorResponse.code === 1) {
@@ -168,28 +168,44 @@ export default {
                   } catch (er) {
                     // ...
                   }
-                  if ('default' in sessionData && analyticsEnv === 'production') {
+                  if ('default' in sessionData) {
                     const acc = sessionData[sessionData.default];
-
-                    mixpanel.people.set_once({
-                      $email: acc.user_email,
-                      $phone: acc.user_phone,
-                      'Account Type': acc.default === 'peer' ? 'Personal' : 'Business',
-                      $name: acc.user_name,
-                      'Client Type': 'Web Platform',
+                    this.$apm.setUserContext({
+                      id: acc.user_id,
+                      username: acc.user_name,
+                      email: acc.user_email,
                     });
+                    if (analyticsEnv === 'production') {
+                      mixpanel.people.set_once({
+                        $email: acc.user_email,
+                        $phone: acc.user_phone,
+                        'Account Type': acc.default === 'peer' ? 'Personal' : 'Business',
+                        $name: acc.user_name,
+                        'Client Type': 'Web Platform',
+                      });
 
-                    // login identify
-                    mixpanel.identify(acc.user_email);
+                      // login identify
+                      mixpanel.identify(acc.user_email);
 
-                    // track login
-                    mixpanel.track('User Login', {
-                      'Account Type': acc.default === 'peer' ? 'Personal' : 'Business',
-                      'Last Login': new Date(),
-                      'Client Type': 'Web Platform',
-                    });
+                      // track login
+                      mixpanel.track('User Login', {
+                        'Account Type': acc.default === 'peer' ? 'Personal' : 'Business',
+                        'Last Login': new Date(),
+                        'Client Type': 'Web Platform',
+                      });
+                    }
                   }
-                  this.$router.push('/orders');
+                  //check for redirect status before push
+                  let redirectStatus = this.$store.getters.getRedirectStatus;
+                  let redirectOrder = this.$store.getters.getRedirectOrder;
+                  if (redirectStatus) {
+                    //reset redirect status
+                    this.$store.commit('setRedirectStatus', false);
+                    this.$store.commit('setRedirectOrder', '');
+                    this.$router.push(`/orders/tracking/${redirectOrder}`);
+                  } else {
+                    this.$router.push('/orders');
+                  }
                 }
               } catch (error) {
                 // @todo Log the error (central logging)
@@ -197,10 +213,10 @@ export default {
               }
             }
           },
-          (error) => {
+          error => {
             this.login_text = 'Login';
             this.doNotification(2, 'Login failed', 'Login failed. Please try again');
-          },
+          }
         );
       } else {
         this.message = 'Provide all values';
@@ -221,108 +237,108 @@ export default {
 
 <style lang="css">
 .log-item {
-    text-align: center;
-    border: 0px solid #ccc;
-    margin: 5px;
+  text-align: center;
+  border: 0px solid #ccc;
+  margin: 5px;
 }
 
 .sign-inner {
-    max-width: 22rem;
-    border: 1px solid #d8dfe6;
-    border-radius: 4px;
-    margin: 2rem auto;
-    padding: 2rem;
-    font-family: "Helvetica Nueu", "Helvetica", "Arial", "sans-serif";
+  max-width: 22rem;
+  border: 1px solid #d8dfe6;
+  border-radius: 4px;
+  margin: 2rem auto;
+  padding: 2rem;
+  font-family: 'Helvetica Nueu', 'Helvetica', 'Arial', 'sans-serif';
 }
 
 .sign-top {
-    font-size: 1.3rem;
-    line-height: 1.7em;
-    font-weight: 500;
-    text-align: center;
-    color: #666;
+  font-size: 1.3rem;
+  line-height: 1.7em;
+  font-weight: 500;
+  text-align: center;
+  color: #666;
 }
 
 .sign-in-button {
-    width: 90%;
-    margin: 1em;
-    background-color: #30487b;
-    color: #fff;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    font-size: initial;
-    letter-spacing: 1.1px;
-    border-radius: 5px;
-    text-align: center;
-    cursor: pointer;
+  width: 90%;
+  margin: 1em;
+  background-color: #30487b;
+  color: #fff;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  font-size: initial;
+  letter-spacing: 1.1px;
+  border-radius: 5px;
+  text-align: center;
+  cursor: pointer;
 }
 
 .sign-text {
-    padding-top: 5px;
-    padding-bottom: 5px;
-    font-size: large;
-    margin-bottom: 10px;
-    text-align: center;
-    color: #666;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  font-size: large;
+  margin-bottom: 10px;
+  text-align: center;
+  color: #666;
 }
 
 .sign-holder {
-    margin: 1em;
-    display: block;
+  margin: 1em;
+  display: block;
 }
 
-.sign-holder input:not([type="checkbox"]) {
-    width: 100%;
+.sign-holder input:not([type='checkbox']) {
+  width: 100%;
 }
 
 .sign-holder .sign-holder__input {
-    -webkit-box-shadow: none;
-    -moz-box-shadow: none;
-    box-shadow: none;
-    height: 30px;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+  height: 30px;
 }
 
 .sign-smaller {
-    font-size: 14px;
-    color: #999999;
+  font-size: 14px;
+  color: #999999;
 }
 
 .sign-forgot-pass,
 .sign-in-sign-up {
-    text-align: center;
+  text-align: center;
 }
 
 .sign-buttom__img {
-    vertical-align: middle;
-    width: 14px;
-    padding-bottom: 3px;
-    margin-right: 10px;
+  vertical-align: middle;
+  width: 14px;
+  padding-bottom: 3px;
+  margin-right: 10px;
 }
 
 .sign-holder__link {
-    color: #1782c5;
-    text-decoration: none;
+  color: #1782c5;
+  text-decoration: none;
 }
 
 .dimen {
-    width: 83%;
+  width: 83%;
 }
 
 .sign-holder__pointer {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 .sign-form {
-    height: 42px !important;
-    width: 110% !important;
+  height: 42px !important;
+  width: 110% !important;
 }
 
 .sign-in-error {
-    color: #e08445;
-    font-family: "Rubik", sans-serif;
+  color: #e08445;
+  font-family: 'Rubik', sans-serif;
 }
 
 .sign-btn-color {
-    border-width: 0px !important;
+  border-width: 0px !important;
 }
 </style>
