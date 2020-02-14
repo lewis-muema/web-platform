ab07efdc60eed563ac3c84c4346ed86cd68ba594
Clinton Kimathi <xpharicy@rocketmail.com>
2020-02-14 06:45:09 +0000

    Revert "Merge branch 'dev' into 'staging'"
    This reverts merge request !1513

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preserveWhitespace: false,
   postcss: [
     require('autoprefixer')({
-      overrideBrowserslist: ['last 3 versions'],
+      browsers: ['last 3 versions'],
     }),
   ],
 };
