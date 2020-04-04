3efa5d8eed74627c2971552e1e672665a3d15c44
daudi <daudi@sendyit.com>
2020-04-04 06:29:52 +0000

    Added some mixpanel tracking for some order placement events
    (cherry picked from commit b49dbbc9ab201082f519dcbfdf741a3720d6bf19)

--- src/modules/orders/_components/order_placement/OrderPlacement.vue
+++ src/modules/orders/_components/order_placement/OrderPlacement.vue
@@ -310,6 +310,8 @@ export default {
     },
 
     addExtraDestinationWrapper() {
+      const extradestination = 'Extra Destination'; 
+      this.mixpanelTrackOrderLocations(extradestination)
       this.addExtraDestination();
       this.scrollToBottom();
     },
@@ -408,6 +410,7 @@ export default {
           eventLabel: 'Pickup Location - Order Placement - Web App',
         };
         this.fireGAEvent(eventPayload);
+        this.mixpanelTrackOrderLocations(eventPayload.eventLabel);
       } else {
         const eventPayload = {
           eventCategory: 'Order Placement',
@@ -415,9 +418,15 @@ export default {
           eventLabel: 'Destination Location - Order Placement - Web App',
         };
         this.fireGAEvent(eventPayload);
+        this.mixpanelTrackOrderLocations(eventPayload.eventLabel);
       }
       this.attemptPriceRequest();
     },
+
+    mixpanelTrackOrderLocations(locationtype) {
+      this.trackMixpanelEvent(`Successfully set Order ${locationtype}`);
+    },
+    
     attemptPriceRequest() {
       if (
         Array.isArray(this.locations)
