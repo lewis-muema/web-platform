03a9a8c6f187de82d6eaf32d056eef11fdcb4053
lewism <lewis@sendy.co.ke>
2020-05-22 18:03:50 +0300

    Cater for order completion on no destination orders

--- src/modules/orders/_components/order_placement/_components/PaymentOptions.vue
+++ src/modules/orders/_components/order_placement/_components/PaymentOptions.vue
@@ -1094,9 +1094,6 @@ export default {
       payload = {
         values: payload,
       };
-      if (this.$route.path === '/orders/dedicated/multi-destination') {
-        payload.mode = 'multi-destination';
-      }
       this.identifyMixpanelUser(acc.user_email);
       return payload;
     },
