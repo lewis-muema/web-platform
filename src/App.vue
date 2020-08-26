8c69adb4c014c5bf3dfddbe6768c533435c33783
lewism <lewis@sendy.co.ke>
2020-08-26 12:27:21 +0300

    Auto popup beacon and redirect mobile website to respective apps using deep links

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,7 @@ export default {
   computed: {
     ...mapGetters({
       getSession: 'getSession',
+      getPickUpFilledStatus: 'getPickUpFilledStatus',
     }),
     notification_status() {
       return this.$store.getters.getNotificationStatus;
@@ -48,6 +49,11 @@ export default {
     getSession(val) {
       if (val) {
         this.updateFirebaseToken();
+      }
+    },
+    $route(to, from) {
+      if (to.path === '/auth' || to.path === '/auth/sign_in' || to.path === '/orders') {
+        this.autoPopBeacon();
       }
     },
   },
@@ -91,6 +97,9 @@ export default {
       // initilize firebase on load
       this.initializeFirebase();
       this.loadFCMListeners();
+      this.detectAndroid();
+      this.detectIOS();
+      this.autoPopBeacon();
     }
   },
   methods: {
@@ -288,6 +297,53 @@ export default {
       }
       // reset notification status
       this.$store.commit('setNotificationStatus', false);
+    },
+
+    detectAndroid() {
+      if (navigator.userAgent.match(/Android/i)) {
+        const notification = {
+          title: 'Mobile redirect',
+          level: 2,
+          message: 'We have detected you are using an android device. We will redirect you to the android app in the next few seconds for the best experience',
+        };
+        this.$store.commit('setNotification', notification);
+        this.$store.commit('setNotificationStatus', true);
+        setTimeout(() => {
+          window.location = ' https://www.sendyit.com/android_customer_app/';
+        }, 10000);
+      }
+    },
+    detectIOS() {
+      if (navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i)) {
+        const notification = {
+          title: 'Mobile redirect',
+          level: 2,
+          message: 'We have detected you are using an IOS device. We will redirect you to the IOS app in the next few seconds for the best experience',
+        };
+        this.$store.commit('setNotification', notification);
+        this.$store.commit('setNotificationStatus', true);
+        setTimeout(() => {
+          window.location = 'itms://itunes.apple.com/us/app/sendy-delivery-app/id1088688361?mt=8';
+        }, 10000);
+      }
+    },
+    autoPopBeacon() {
+      setTimeout(() => {
+        if (this.$route.path === '/auth' || this.$route.path === '/auth/sign_in') {
+          window.Beacon('open');
+          window.Beacon('navigate', '/answers/');
+          setTimeout(() => {
+            window.Beacon('suggest', ['59d5bc412c7d3a40f0ed346c']);
+          }, 3000);
+        }
+        if (this.$route.path === '/orders' && !this.getPickUpFilledStatus) {
+          window.Beacon('open');
+          window.Beacon('navigate', '/answers/');
+          setTimeout(() => {
+            window.Beacon('suggest', ['59d6021a042863379ddc6c61']);
+          }, 3000);
+        }
+      }, 300000);
     },
   },
 };
