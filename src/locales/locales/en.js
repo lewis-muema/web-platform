1012df2e678b24f2689c719aa8c95583a2264846
victor misiko <victormisiko.vm@gmail.com>
2021-02-09 13:04:56 +0300

    Changed the format of locales Files

--- src/locales/locales/en.js
+++ src/locales/locales/en.js
@@ -1,13 +1,13 @@
 
-import byPassLogin from '../en/modules/auth/components/byPassLogin';
-import forgetPassword from '../en/modules/auth/components/forgetPassword';
-import loadingComponent from '../en/modules/auth/components/loadingComponent';
-import resetPassword from '../en/modules/auth/components/resetPassword';
-import signIn from '../en/modules/auth/components/signIn';
-import signUp from '../en/modules/auth/components/signUp';
-import signUpDetails from '../en/modules/auth/components/signUpDetails';
-import signUpVerification from '../en/modules/auth/components/signUpVerification';
-import successComponent from '../en/modules/auth/components/successComponent';
+import byPassLogin from '../modules/auth/en/components/byPassLogin';
+import forgetPassword from '../modules/auth/en/components/forgetPassword';
+import loadingComponent from '../modules/auth/en/components/loadingComponent';
+import resetPassword from '../modules/auth/en/components/resetPassword';
+import signIn from '../modules/auth/en/components/signIn';
+import signUp from '../modules/auth/en/components/signUp';
+import signUpDetails from '../modules/auth/en/components/signUpDetails';
+import signUpVerification from '../modules/auth/en/components/signUpVerification';
+import successComponent from '../modules/auth/en/components/successComponent';
 
 const translations = {
   byPassLogin,
