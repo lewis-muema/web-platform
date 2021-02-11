6ef7b7b0a63d61b79e8c0e175acbdc6facba1359
victor misiko <victormisiko.vm@gmail.com>
2021-02-11 23:22:49 +0300

    Replaced strings in ApprovalDialogue

---
+++ src/locales/orders/en/components/social_media/approvalDialogue.js
@@ -0,0 +1,12 @@
+const en = {
+  your_application_has_been_recieved: 'Your application has been received',
+  by_filling_social_commerce_business: 'By filling out that you are a social commerce business, you will be enrolled to theSendy Social Club (after verification).',
+  enjoy_discounted_prices: 'You will enjoy discounted prices, exclusive trainings, events and other benefits intended to help you grow your business.',
+  congrats_your_application_has_been_approved:'Congrats! your application to join the Sendy Social Club has been approved. You will now enjoy discounted prices on your orders among other benefits.',
+  your_application_has_been_declined: 'Your application has been declined',
+  your_application_has_been_approved:'Your application has been approved',
+  sorry_your_application_declined: 'Sorry, your application to join the Sendy social club has been declined. Your business doesn’t qualify as a social commerce business, however, you can still place Sendy orders',
+
+};
+
+export default en;
