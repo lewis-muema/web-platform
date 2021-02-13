70e060bddc7d8168339623cc143b28483a1c00d7
victor misiko <victormisiko.vm@gmail.com>
2021-02-13 15:34:50 +0300

    replaced string in Transactions component

--- src/locales/general/en.js
+++ src/locales/general/en.js
@@ -854,7 +854,9 @@ const translations = {
   drop_off_lowercase: 'drop off',
   no_notes_provided: 'No notes provided.',
   matched_partner_to_package: '',
-
+  valid_promo_codes: 'Valid Promo Codes',
+  off: 'OFF',
+  max: 'Max',
 
 };
 
--- src/locales/general/fr.js
+++ src/locales/general/fr.js
@@ -827,6 +827,9 @@ const translations = {
   pickup_lowercase: 'FR_pick up',
   drop_off_lowercase: 'FR_drop off',
   no_notes_provided: 'FR_No notes provided.',
+  valid_promo_codes: 'Valid Promo Codes',
+  off: 'FR)OFF',
+  max: 'FR_Max',
 
 
 };
