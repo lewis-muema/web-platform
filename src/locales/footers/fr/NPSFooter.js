670d6435c98edbf4aaf3363ca0a2c4864635e59a
victor misiko <victormisiko.vm@gmail.com>
2021-02-16 09:37:23 +0300

    Added footer resource fles

---
+++ src/locales/footers/fr/NPSFooter.js
@@ -0,0 +1,31 @@
+
+const fr = {
+  not_likely: 'Bonojour',
+  very_likely: 'Bonjour 10 Very Likely',
+  submit: 'Bonojour',
+  how_likely: 'FR-How likely are you to recommend a friend or colleague to use Sendy?',
+  like_most: 'FR-What do you like most about Sendy? (Optional)',
+  thank_you: 'FR-Thank you. We will use your feedback to improve our service',
+  transportation: 'Bonojour',
+  freight: 'Bonojour',
+  helpline: 'Bonojour',
+  hi: 'Bonojour',
+  menu: 'Bonojour',
+  switch_to: 'Bonojour',
+  personal: 'Bonojour personal',
+  account: 'Bonojour account',
+  create_business_account: ' FrCreate Business Account',
+  new_delivery: 'FR-New Delivery',
+  payment: 'FR-payment',
+  orders: 'FR-Orders',
+  settings: 'FR-Settings',
+  analytics: 'FR-analytics',
+  profile: 'FR-Profile',
+  logout: ' FR-Logout',
+  business: 'FR_Business',
+  business_acc: '(FR-Business Acc)',
+  personal_acc: '(FR-Personal Acc)',
+
+};
+
+export default fr;
