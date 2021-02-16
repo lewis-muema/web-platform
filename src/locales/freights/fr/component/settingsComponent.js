0b22dd0abce61ba145e3c6d4b0d53af24cfeeee8
victor misiko <victormisiko.vm@gmail.com>
2021-02-16 09:52:52 +0300

    Added freight module resource files

---
+++ src/locales/freights/fr/component/settingsComponent.js
@@ -0,0 +1,27 @@
+
+const fr = {
+  manage_approvers: 'FR_Manage Approvers',
+  add_approver: 'FR_Add approver',
+  name: 'FR_Name',
+  phone: 'FR_Phone',
+  email: 'FR_Email',
+  status: 'FR_Status',
+  active: 'FR_Active',
+  action: 'FR_Action',
+  remove: 'FR_Remove',
+  select_user: 'FR_Select user to approve freight orders',
+  approve: 'FR_Approve',
+  fetch_approvers: 'FR_Fetching approvers list',
+  no_approvers: 'FR_No approvers available',
+  approver_updated: 'FR_Approver updated!',
+  approver_removed: 'FR_Approver removed successfully',
+  failed_remove_approver: 'FR_Failed to remove approver!',
+  failed_remove_approver_text: 'FR_Failed to remove approver, Kindly retry again or contact customer support',
+  approver_updated_successfully: 'FR_approver updated successfully',
+  failed_add_approver: 'FR_Failed to add approvers!',
+  failed_add_approver_text: 'FR_Failed to add approvers, Kindly retry again or contact customer support',
+  failed_add_approver_single: 'FR_Failed to add approvers, Kindly select a single user',
+};
+
+
+export default fr;
---
+++ src/locales/freights/fr/verifyComponent.js
@@ -0,0 +1,14 @@
+
+const fr = {
+  terms_of_service: 'FR_Our terms of services',
+  acknowledge: 'FR_acknowledge that i have read and understood the terms of service',
+  application_submitted: 'FR_Your Application has been submitted',
+  thanks: 'FR_Thanks {logged_user} we have received your registration.',
+  customer_rep: 'FR_Our customer representative will contact you within 24 hrs with the next steps to activate your account and get you started.',
+  something_went_wrong: 'FR_Something went wrong.',
+  freight_final_setup: 'FR_Freight final setup failed',
+  agree_terms_condition: 'FR_Agree to Terms and Conditions',
+  terms_conditions_accepted: 'FR_Terms and Conditions accepted!',
+};
+
+export default fr;
