6859afb1e2783cd3cb048d63097ac4fa0790193b
joyce.k <joyce@sendyit.com>
2021-02-24 12:31:18 +0300

    route change payment methods

--- src/modules/payment/_components/PayMethod.vue
+++ src/modules/payment/_components/PayMethod.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="paymethod">
-    <router-link class="paymethod--link menu-links" :key="method.Payment_method_id" v-for="method in payment_methods" :to="`/payment/${method.name.replace(/\s/g, '').toLowerCase()}`">
+    <router-link class="paymethod--link menu-links" :key="method.Payment_method_id" v-for="method in payment_methods" :to="`/payment/${method.name.replace(/-|\s/g, '').toLowerCase()}`">
     {{ method.name }}
 </router-link>
   </div>
