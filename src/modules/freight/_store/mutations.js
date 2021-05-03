5a449d117560bc8f772dfd63a41a96cd3b41f793
Clinton Mutindi <clinton@sendy.co.ke>
2021-05-03 11:45:49 +0300

    Freight : Document Management
    - Restructure component layout to facilitate display of various freight 
    documents.

--- src/modules/freight/_store/mutations.js
+++ src/modules/freight/_store/mutations.js
@@ -3,4 +3,22 @@ export default {
   setFreightOrders(state, orders) {
     state.freight_orders = orders;
   },
+  setDocumentUrl(state, val) {
+    state.document_url = val;
+  },
+  setDocumentName(state, val) {
+    state.document_name = val;
+  },
+  setDocumentDialog(state, val) {
+    state.document_dialog = val;
+  },
+  setOrderDetail(state, val) {
+    state.order_id = val;
+  },
+  setDeclineDocument(state, val) {
+    state.decline_doc = val;
+  },
+  setDocumentDialogDocument(state, val) {
+    state.decline_dialog = val;
+  },
 };
