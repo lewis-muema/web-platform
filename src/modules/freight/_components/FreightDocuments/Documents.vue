e6cc0d71faf2c5fd26cbdefd01c4a21c4d357e94
Clinton Mutindi <clinton@sendy.co.ke>
2021-05-07 10:39:41 +0300

    Freight : Loading Docs
    - Allow atleast a minimum of one loading docs to be uploaded

--- src/modules/freight/_components/FreightDocuments/Documents.vue
+++ src/modules/freight/_components/FreightDocuments/Documents.vue
@@ -450,8 +450,8 @@ export default {
       if (docsOptionsLength !== submittedDocsLength) {
         this.doNotification(2, 'Kindly upload all the necessary loading document(s)', '');
       } else {
-        const filtered = this.billOfLandingName.find(location => location.name === '');
-        if (filtered === undefined || filtered === 'undefined') {
+        const filtered = this.billOfLandingName.find(location => location.name !== '');
+        if (filtered !== undefined && filtered !== 'undefined') {
           this.processRequest();
         } else {
           this.doNotification(2, 'Kindly upload all the necessary loading document(s)', '');
@@ -462,14 +462,17 @@ export default {
       const documentUpload = [];
 
       for (let i = 0; i < this.billOfLandingName.length; i++) {
-        documentUpload.push({
-          document_type: this.billOfLandingName[i].type,
-          document_name: this.billOfLandingName[i].doc_name,
-          url: `https://sendy-partner-docs.s3-eu-west-1.amazonaws.com/${
-            this.billOfLandingName[i].name
-          }`,
-        });
+        if (this.billOfLandingName[i].name !== '') {
+          documentUpload.push({
+            document_type: this.billOfLandingName[i].type,
+            document_name: this.billOfLandingName[i].doc_name,
+            url: `https://sendy-partner-docs.s3-eu-west-1.amazonaws.com/${
+              this.billOfLandingName[i].name
+            }`,
+          });
+        }
       }
+
       let acc = {};
       const session = this.$store.getters.getSession;
       if ('default' in session) {
