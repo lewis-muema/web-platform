29fc1ae4935479c3f025748558f3f7318d365c39
lewism <lewis@sendy.co.ke>
2021-05-17 11:20:49 +0300

    Fix lint issue

--- src/modules/orders/_components/tracking/_store/actions.js
+++ src/modules/orders/_components/tracking/_store/actions.js
@@ -69,7 +69,9 @@ const cancelOrder = function cancelOrder({ dispatch }, data) {
 const trackMQTT = function trackMQTT({ commit, state }) {
   if (state.tracking_data.confirm_status > 0) {
     const trackingNo = state.tracking_data.rider.phone_no_1;
-    const cityCode = this.getters.getClosestCity ? state.tracking_data.city_code : this.getters.getClosestCity;
+    const cityCode = this.getters.getClosestCity
+      ? state.tracking_data.city_code
+      : this.getters.getClosestCity;
 
     const uri = `${cityCode}/${trackingNo}`;
 
