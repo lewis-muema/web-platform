d1f2e00efd975f723910e032630e64770e3e9061
Clinton Mutindi <clinton@sendy.co.ke>
2021-06-16 12:11:08 +0300

    Freight : Tracking
    - Clean CSS and set up tracking layout

--- src/modules/freight/_components/tracking/ShipmentVehicles.vue
+++ src/modules/freight/_components/tracking/ShipmentVehicles.vue
@@ -9,6 +9,8 @@
         v-for="val in vehicles"
         :key="val.id"
         class="ongoing-trucks-card"
+        :class="{ active: active_shipment(val.id) }"
+        @click="trackShipment(val)"
       >
         <div class="ongoing-trucks-details-container">
           <img
@@ -42,10 +44,46 @@ export default {
         },
         {
           id: 4,
-          reg_no: 'KCY 233G',
+          reg_no: 'KCY 234G',
+        },
+        {
+          id: 5,
+          reg_no: 'KCY 235G',
+        },
+        {
+          id: 6,
+          reg_no: 'KCY 236G',
+        },
+        {
+          id: 7,
+          reg_no: 'KCY 237G',
+        },
+        {
+          id: 8,
+          reg_no: 'KCY 238G',
+        },
+        {
+          id: 9,
+          reg_no: 'KCY 240G',
+        },
+        {
+          id: 10,
+          reg_no: 'KCY 241G',
         },
       ],
+      set_shipemnt_id: '',
     };
+  },
+  methods: {
+    active_shipment(shipmentNo) {
+      if (this.set_shipemnt_id === shipmentNo) {
+        return true;
+      }
+      return false;
+    },
+    trackShipment(shipment) {
+      this.set_shipemnt_id = shipment.id;
+    },
   },
 };
 </script>
