cbd8f4ff14e4f014cd3978c524e7fd3033c87924
Jeremy <jeremywambugu@sendyit.com>
2021-07-12 15:43:51 +0300

    language support updates

--- src/modules/freight/_components/OrdersDetailsComponent.vue
+++ src/modules/freight/_components/OrdersDetailsComponent.vue
@@ -13,7 +13,7 @@
           {{ $t('orderDetailsComponent.back') }}
         </div>
         <div class="shipment-id-label">
-          Shipment ID : {{ freightOrderDetail.transformed_id }}
+          {{ $t('orderDetailsComponent.shipment_id') }} : {{ freightOrderDetail.transformed_id }}
         </div>
         <div
           v-if="view_tracking"
