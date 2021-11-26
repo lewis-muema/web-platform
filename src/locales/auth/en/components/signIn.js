14a1750e2baeed47eeeb312d83411e9449ba4324
Dorcas <dorcas@Dorcass-MacBook-Pro.local>
2021-11-26 16:56:33 +0300

    phrase setup and initilialization

--- src/locales/auth/en/components/signIn.js
+++ src/locales/auth/en/components/signIn.js
@@ -1,22 +1,6 @@
 
+import * as en from './signIn.json';
 
-const en = {
-  login_sendy: 'Log in to Sendy',
-  enter_email: 'Enter Email',
-  password: 'Password',
-  forgot_password: 'Forgot password?',
-  have_account: "Don't have an Account?",
-  sign_up: 'Sign Up',
-  login: 'Login',
-  view: 'View Page - Sign In - Web Platform',
-  login_status: 'Logging in ...',
-  login_failed: 'Login failed',
-  wrong_password: 'Wrong password or email.',
-  account_deactivated: 'Account deactivated',
-  login_failed_text: 'Login failed. Please try again',
-  provide_all: 'Provide all values',
-
-};
+export default en.default;
 
 
-export default en;
