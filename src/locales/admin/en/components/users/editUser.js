affee716b4e7604e5dc0add8bd8d6eed9c1c1eb5
Dorcas <dorcas@Dorcass-MacBook-Pro.local>
2021-11-29 17:25:24 +0300

    admin component

--- src/locales/admin/en/components/users/editUser.js
+++ src/locales/admin/en/components/users/editUser.js
@@ -1,20 +1,3 @@
-const en = {
-  edit_user: 'Edit User',
-  name: 'Name',
-  email: 'Email',
-  phone: 'Phone',
-  department: 'Department',
-  status: 'Status',
-  user_type: 'User Type',
-  submit: 'submit',
-  update: 'Update',
-  active: 'Active',
-  deactivate: 'Deactivate',
-  normal: 'Normal',
-  admin: 'Admin',
-  edit_successful: 'Edit Successful!',
-  something_went_wrong: 'Something went wrong.',
+import * as en from './editUser.json';
 
-};
-
-export default en;
+export default en.default;
